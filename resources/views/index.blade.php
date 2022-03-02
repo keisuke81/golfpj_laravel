3a757734e86792bb498d475e83060010c732d136
keisuke <n.kei.0801.1997@gmail.com>
2022-03-03 06:49:37 +0900

    heroku_upload3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 <html lang="ja">
 
 <head>
-  <link href="{{asset('css/style.css')}}" rel="stylesheet">
-  <link href="{{asset('/css/reset.css')}}" rel="stylesheet">
+  <link href="../../public/css/style.css" rel="stylesheet">
+  <link href="../../public/css/reset.css" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
