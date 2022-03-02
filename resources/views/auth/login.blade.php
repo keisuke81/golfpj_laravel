7870e309b12751f4e008bf9e9e7b9381eef232f6
keisuke <n.kei.0801.1997@gmail.com>
2022-03-03 06:31:49 +0900

    heroku_upload3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,7 +64,7 @@
 
                                 @if (Route::has('password.request'))
                                 <div>
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a class="btn_small btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
                                 </a>
                                 </div>
