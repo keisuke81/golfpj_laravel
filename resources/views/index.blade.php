c13c4889933ca0ff261de73637b17900ca340294
keisuke <n.kei.0801.1997@gmail.com>
2022-03-05 20:19:46 +0900

    logout_add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
   @component('components.menu')
   @endcomponent
   <div class="wrapper">
-    <a class="btn" href="/register">新規登録する</a>
-    <a class="btn" href="/login">ログインする</a>
+    <a class="btn" href="/register">新規登録</a>
+    <a class="btn" href="/login">ログイン</a>
   </div>
 
 
