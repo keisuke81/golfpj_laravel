9d0d6b819f138407e7e25cfdc80611d1c433c532
keisuke <n.kei.0801.1997@gmail.com>
2022-03-05 14:41:01 +0900

    head_tag_component

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-
-<head>
-  <title>Pusher Test</title>
-  <link href="{{asset('css/style.css')}}" rel="stylesheet">
-  <link href="{{asset('css/reset.css')}}" rel="stylesheet">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-</head>
+@component('components.header')
+@endcomponent
 
 <body onload="init()" onunload="init2()">
 
--- resources/views/offer.blade.php
+++ resources/views/offer.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <link href="{{asset('css/style.css')}}" rel="stylesheet">
-  <link href="{{asset('css/reset.css')}}" rel="stylesheet">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-  <title>offer_page</title>
-</head>
+@component('components.header')
+@endcomponent
 
 <body>
   @component('components.menu')
