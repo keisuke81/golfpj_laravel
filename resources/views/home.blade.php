72a4efe76b3b8ccc23f30edd5e4d2ecdd08a2f6a
keisuke <n.kei.0801.1997@gmail.com>
2022-03-09 21:01:57 +0900

    linelogin_test7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,4 @@
 <<<<<<< HEAD
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
 =======
 <!DOCTYPE html>
 <html lang="ja">
@@ -39,5 +20,5 @@
 >>>>>>> 1e76a731c3cbe6d4c0f4a4bdece00b4945d34a1e
         </div>
     </div>
-</div>
-@endsection
+</body>
+
