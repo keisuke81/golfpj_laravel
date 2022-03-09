d2f045351c6e74213c524f935a909d2c6112ad30
keisuke <n.kei.0801.1997@gmail.com>
2022-03-09 21:55:14 +0900

    linelogin_test12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
 
             @if (Route::has('register'))
             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+
+            <a href="{{route('linelogin')}}">LINEログイン</a>
             @endif
             @endauth
         </div>
