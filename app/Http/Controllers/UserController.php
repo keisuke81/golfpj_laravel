da47b0ac66a96660f6ff258b7ae5d42b8dbf07ee
keisuke <n.kei.0801.1997@gmail.com>
2022-03-12 18:29:03 +0900

    auth_test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
     public function getRegistrationInformation(User $user_id){
 
         $user_id = Auth::id();
+        dd($user_id);
         $registration = User::where('id', $user_id)->first();
 
         return view('registration_information')->with([
