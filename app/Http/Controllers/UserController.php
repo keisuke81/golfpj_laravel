e0cfe8e463e5bc27de5da22ee24b6b7feb0cfb83
keisuke <n.kei.0801.1997@gmail.com>
2022-03-12 23:52:53 +0900

    auth_test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,13 +35,14 @@ class UserController extends Controller
 
         return view('registration_information')->with([
             'registration' => $registration,
-            
+
         ]);
     }
     //登録情報更新ページの表示//
     public function profile_edit(User $user_id){
         $user_id = Auth::id();
         $registration = User::where('id', $user_id)->first();
+        dd($registration);
 
         return view('profile_edit')->with([
             'registration' => $registration,
