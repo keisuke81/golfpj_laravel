bc700c5bdc7f3a3166ee8fbc42864d4096ef14e1
keisuke <n.kei.0801.1997@gmail.com>
2022-03-13 07:40:17 +0900

    auth_test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,6 +51,7 @@ class UserController extends Controller
     public function profile_update($id, Request $request)
     {
         $user_id =$id;
+        dd($user_id);
 
         $param = [
             'nickname'=>$request->nickname,
