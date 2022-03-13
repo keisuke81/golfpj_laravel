030a3cc0774043234d0ec32bb0e9b2d1b9021624
keisuke <n.kei.0801.1997@gmail.com>
2022-03-13 11:31:27 +0900

    auth_test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,7 @@
             @endif
             @endauth
         </div>
+        <a href="{{route('line.login')}}">LINEログイン</a>
         @endif
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
