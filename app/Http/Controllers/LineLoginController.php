a1414348de7c13b25d87ffec4f55e35040ca79aa
keisuke <n.kei.0801.1997@gmail.com>
2022-03-16 22:44:23 +0900

    auth_test

--- app/Http/Controllers/LineLoginController.php
+++ app/Http/Controllers/LineLoginController.php
@@ -98,7 +98,7 @@ class LineLoginController extends Controller
 
         // あったらログイン
         if (!empty($user)) {
-            return view('login')->with(['user_id' => $user_id]);
+            return view('login');
 
             // なければ登録してからログイン
         } else {
@@ -108,7 +108,7 @@ class LineLoginController extends Controller
             $user->name = $profile->displayName;
             $user->save();
             Auth::id();
-            return view('login')->with(['user_id' => $user_id]);
+            return view('login');
         }
     }
 }
