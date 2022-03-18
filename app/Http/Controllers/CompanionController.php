833e031aaf520665bcfc856baaf6920cfa9d39a2
keisuke <n.kei.0801.1997@gmail.com>
2022-03-19 00:39:01 +0900

    auth_Test

--- app/Http/Controllers/CompanionController.php
+++ app/Http/Controllers/CompanionController.php
@@ -71,7 +71,7 @@ class CompanionController extends Controller
         Follow::create($param);
 
 
-        return redirect('profile/{id}/{user_id}');
+        return redirect()->route('getDetail');
     }
 
     //お気に入り解除//
