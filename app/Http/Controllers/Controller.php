030f039e5f41164f88f8b948fec0d73abaa4c0a7
keisuke <n.kei.0801.1997@gmail.com>
2022-03-19 12:37:27 +0900

    auth_test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,5 +11,8 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+  public function __construct(){
+    return redirect()->route('linelogin');
+  }
     
 }
