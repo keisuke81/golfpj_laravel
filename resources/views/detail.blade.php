2969d09ab820e4f406189da8bc5dbc8de6975f8c
keisuke <n.kei.0801.1997@gmail.com>
2022-03-19 16:39:52 +0900

    auth_test

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -26,9 +26,9 @@
 
       <div class="follows">
         @if($item->is_followed_by_auth_user())
-        <a href="{{ route('noFollow', ['id' => $item->id, 'user_id' => $user_id]) }}" class="btn black">フォロー解除</a>
+        <a href="{{ route('noFollow', ['id' => $item->id]) }}" class="btn black">フォロー解除</a>
         @else
-        <a href="{{ route('getFollow', ['id' => $item->id, 'user_id' => $user_id]) }}" class="btn white">フォローする</a>
+        <a href="{{ route('getFollow', ['id' => $item->id]) }}" class="btn white">フォローする</a>
         @endif
       </div>
 
